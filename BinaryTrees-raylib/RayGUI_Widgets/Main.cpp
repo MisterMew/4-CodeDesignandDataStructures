d61cd4d503b077b7eacff268c0a2279a88310a22
MisterMew <thebesteccentric@gmail.com>
2021-06-29 19:08:42 +1000

    Binary Trees Final Commit
    Merging the files to create the final submission project
    - Removed the secondary project

--- BinaryTrees-raylib/RayGUI_Widgets/Main.cpp
+++ BinaryTrees-raylib/RayGUI_Widgets/Main.cpp
@@ -1,8 +1,20 @@
-#include "Main.h"
-#include "BinaryTreeCore.h"
-#include "TreeNodeCore.h"
+#include <iostream>
 
- /// INITIALISATION
+using namespace std;
+
+#define RAYGUI_IMPLEMENTATION
+#define RAYGUI_SUPPORT_ICONS
+#define RAYGUI_STATIC
+
+#include "raygui.h"
+#include "BinaryTree.h"
+
+#undef RAYGUI_IMPLEMENTATION
+
+int valueBoxValue = 0;
+bool valueBoxEditMode = false;
+
+/// INITIALISATION
 /* Program initialisation */
 void Init() {
 	const int screenWidth = 1280;  //Set screen width
@@ -14,42 +26,41 @@ void Init() {
 	SetExitKey(0);
 }
 
- /// Main
 int main() {
-	Init(); //Initialisation
+	Init();
 
 	BinaryTree binaryTree;
-	TreeNode* selectedNode = nullptr;
 
 	bool exitWindow = false;
 	while (!exitWindow) {
 		exitWindow = WindowShouldClose();
 
-	#pragma region [ Drawing ]
+#pragma region [ Drawing ]
 
 		BeginDrawing();
 
 		ClearBackground(RAYWHITE);
 
-		 /// DRAW OPTIONS
-		/* Draw the menu options to interact with the DLL */
+		if (binaryTree.GetRoot() != nullptr) {
+			binaryTree.Display();
+		}
+
+		/// DRAW OPTIONS
+	   /* Draw the menu options to interact with the DLL */
 		GuiSetStyle(TEXTBOX, TEXT_ALIGNMENT, GUI_TEXT_ALIGN_CENTER);
 
-		if (GuiValueBox(Rectangle{ 5, 15, 125, 30 }, NULL, &valueBoxValue, 0, 100, valueBoxEditMode)) {
-			valueBoxEditMode = !valueBoxEditMode;
-		}
+		GuiValueBox(Rectangle{ 5, 15, 125, 30 }, "INPUT", &valueBoxValue, 0, 100, true);
 
 		if (GuiButton(Rectangle{ 5, 50, 125, 30 }, GuiIconText(RICON_OK_TICK, "INSERT NODE"))) {
-			binaryTree.InsertNode(valueBoxValue);
+			binaryTree.Insert(valueBoxValue);
 		}
 		if (GuiButton(Rectangle{ 5, 85, 125, 30 }, GuiIconText(RICON_CROSS, "DELETE NODE"))) {
-			binaryTree.DeleteNode(valueBoxValue);
+			binaryTree.Delete(valueBoxValue);
 		}
-		binaryTree.DrawRoot(selectedNode);
 
 		EndDrawing();
 
-	#pragma endregion
+#pragma endregion
 	}
 
 	CloseWindow();
